476ca02174be403557655e3c62e4b13f2f846156
LunaAmora <luna.mberry@gmail.com>
2022-12-25 10:39:55 -0400

    Add a fmt config

--- src/lib.rs
+++ src/lib.rs
@@ -10,12 +10,9 @@ pub fn run() {
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
     event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
+        Event::WindowEvent { ref event, window_id } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested |
+            WindowEvent::KeyboardInput {
                 input:
                     KeyboardInput {
                         state: ElementState::Pressed,
